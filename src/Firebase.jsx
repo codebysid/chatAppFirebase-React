2188225849a4a9d989d655f761c2cc3083161ec6
Siddhant <codebysidd@gmail.com>
2023-02-24 20:00:56 +0530

    Cred Remove

--- src/Firebase.jsx
+++ src/Firebase.jsx
@@ -3,17 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCPV_0a_k1szJQE4s_L0_WxU7xNQnHE5_Q",
-
-    authDomain: "chatapp-f5889.firebaseapp.com",
-
-    projectId: "chatapp-f5889",
-
-    storageBucket: "chatapp-f5889.appspot.com",
-
-    messagingSenderId: "483624492988",
-
-    appId: "1:483624492988:web:d0b206ec528cf19e1655b9",
+    //Enter Configuration
 };
 
 const app = initializeApp(firebaseConfig);
